e4f195ad53f06adccd2f48d2f8b91a525f844f3f
Ahmad Rehan <ahmedrehan98@gmail.com>
2024-04-19 08:09:53 +0200

    add our newly created middleware to our routes

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 require("dotenv").config();
 
 // middleware
@@ -10,8 +12,10 @@ app.use(express.json());
 
 // routes
 app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
